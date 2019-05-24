bccb8a8a7bd8ae19ee6bdba942ad674b5ef7ace1
CyberSCT <50979072+CyberSCT@users.noreply.github.com>
2019-05-24 13:02:05 +0700

    Termux

---
+++ spamcall.sh
@@ -0,0 +1,41 @@
+clear
+figlet spamcall | lolcat
+
+echo "▕▔▔▏▕▔▔▏▕▔▔▏▕▔▔▏
+▕▏┈▔▔┈▕▏▕▂▂▏▕▏▕▏
+▕▏╭╮╭╮▕▏▕▔▔▏▕▏▕▏
+▕▏┳╭╮┳▕▏▕▏▕▏▕▏▕▏
+▕▏╰━━╯▕▏▕▏▕▏▕▂▂▏
+▕▏┈▂▂┈▕▏▕▏▕▏▕▔▔▏
+▕▂▂▏▕▂▂▏▕▂▂▏▕▂▂▏" | lolcat
+ echo "===========================================
+nickname   : Mr.SCT / Arief R.H
+Tools      : MR.SCT
+WA         : +62 857-5549-4099
+Facebook   : edm bos sultann
+============================================" | lolcat
+
+echo "[1]spamcall
+[2]spamsms
+[3]exit" | lolcat
+
+sleep 2
+
+echo "[*]pilih nomor   :" | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+pkg install git
+pkg install figlet
+git clone https://github.com/ClayHackerTeam/prankers
+cd prankers
+sh prankers.sh
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+figlet BAGIAN 2 | lolcat
+fi
